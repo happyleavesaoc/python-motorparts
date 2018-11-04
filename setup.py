c6a7437a081fba0d11373c85223b6b8d911139c6
happyleaves <happyleaves.tfr@gmail.com>
2018-11-04 17:26:43 -0500

    bump requests

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='happyleaves',
     author_email='happyleaves.tfr@gmail.com',
     packages=['motorparts'],
-    install_requires=['beautifulsoup4==4.5.1', 'requests==2.12.4'],
+    install_requires=['beautifulsoup4==4.5.1', 'requests>=2.20.0'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
